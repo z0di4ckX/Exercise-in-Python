ccba46773e153de4825e2cef0fbcbc01dac04e46
Wesly <ultimatecracker88@gmail.com>
2019-06-03 15:33:08 -0400

    Exercise ex5

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = "WesCoder"
+my_age = 25
+my_height = 74 #inches
+my_weight = 160
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print("Let's talk about %s." %(my_name))
+print("He's %d inches tall." %(my_height))
+print("He's %d pounds heavy." %(my_weight))
+print("Actually that's not too heavy.")
+print("He's got %s eyes end %s hair" %(my_eyes, my_hair))
+print("His teeth are usually %s depending on the coffee." %(my_teeth))
+
+print("If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight))
