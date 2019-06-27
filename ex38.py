93af0d66b81f064b31f6997f8c83057af8362c12
Wesly <ultimatecracker88@gmail.com>
2019-06-26 23:24:36 -0400

    Exercise ex38

--- ex38.py
+++ ex38.py
@@ -9,3 +9,14 @@ while len(stuff) != 10:
     print("Adding: ", next_one)
     stuff.append(next_one)
     print("There's %d items now." % len(stuff))
+
+print("There we go: ", stuff)
+
+print("Let's do some  things with stuff.")
+
+print(stuff[1])
+print(stuff[-1]) # Whoa! fancy
+print(stuff.pop())
+
+print(' '.join(stuff)) # What? cool?
+print('#'.join(stuff[3:5]))
