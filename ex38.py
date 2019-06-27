d50a51ba9c90f9a5bf08bc09a2db51d24f142327
Wesly <ultimatecracker88@gmail.com>
2019-06-26 23:20:27 -0400

    Exercise ex38

---
+++ ex38.py
@@ -0,0 +1,11 @@
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+print("Wait there's not 10 things in that list, let's fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisebee", "Corn", "Banana", "Gril", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print("Adding: ", next_one)
+    stuff.append(next_one)
+    print("There's %d items now." % len(stuff))
