322b9ebfde059afd9918c80a7b6792e6a73938d6
Wesly <ultimatecracker88@gmail.com>
2019-07-07 14:28:13 -0400

    Added new function

--- ex43.py
+++ ex43.py
@@ -28,7 +28,21 @@ class Death(Scence):
 
 class CentralCorridor(Scence):
     def enter(self):
-        pass
+        print("The Gothons fo Plante Percal #25 have invaded your ship and destroyed")
+        print("your entire crew. You are the last surviving member and your last")
+        print("mission is to get the neutron destruct bomb from the Weapons Armory,")
+        print("put it in the bridge, and blow the ship up after getting into an")
+        print("escape pod.")
+        print("\n")
+        print("You're running  down the central corridor to the Weapons Armory when")
+        print("a Gothons jumps out, red scaly skin, dark grimy teeth, and evil clown costume")
+        print("flowing around his hate filled body. He's blocking the door to the")
+        print("Armory and about to pull a weapon to blast you.")
+
+        action = input()
+
+        if action == "shoot!":
+            print("Quick on the draw you yank out your blaster and fire it at the Gothons")
 
 class LaserWeaponArmory(Scence):
     def enter(self):
