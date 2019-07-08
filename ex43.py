2469194bc59cc91679533d6c2848941f17f63c33
z0di4ckX <ultimatecracker88@gmail.com>
2019-07-08 18:31:26 -0400

    Added new lop in LaserWeaponArmory class

--- ex43.py
+++ ex43.py
@@ -9,7 +9,7 @@ class Scence(object):
 class Engine(object):
     def __init__(self, scene_map):
         self.scene_map = scene_map
-        
+
     def play(self):
         current_scene = self.scene_map.opening_scene()
         while True:
@@ -76,7 +76,24 @@ class CentralCorridor(Scence):
 
 class LaserWeaponArmory(Scence):
     def enter(self):
-        pass
+        print("You do a dive roll into the Weapon Armory, crouch and scan the room")
+        print("for more Gothons that might be hiding. It's dead quiet, too quiet.")
+        print("You stand up and run to the far side of the room and find the")
+        print("neutron bomb in its container. There's a keypad lock on the box")
+        print("and you need the code to get the bomb out. If you get the code")
+        print("worng 10 time then the lock closes forever and you can't")
+        print("get the bomb. The code is 3 digits.")
+        code = "%d%d%d" % (randint(1, 9), randint(1, 9), randint(1, 9))
+        guess = input("[keypad]> ")
+        guesses = 0
+
+        while guess != code and guesses < 10:
+            print("BZZZZZEDDDDD!")
+            guesses += 1
+            guess = input("[keypad]> ")
+
+            if guess == code:
+                print("")
 
 class TheBridge(Scence):
     def enter(self):
